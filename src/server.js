2445d2313383739e9438f0dfe8c2cad3eae67d33
Aulo <138018355+aulolua@users.noreply.github.com>
2023-07-21 14:41:57 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  console.log(req.url,req.socket.remoteFamily,req.socket.remoteAddress,req.socket.remotePort)
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!',
+  }));
+});
+
+server.listen(8000);
