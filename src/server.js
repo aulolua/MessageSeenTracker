9d48f0e0cce4e0bac84d0e472900c97c9856289e
Aulo <138018355+aulolua@users.noreply.github.com>
2023-07-21 14:51:19 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const fs = require('fs');
 
+if (fs.existsSync('./tmp.log')) return console.log('already running');
+
+fs.writeFileSync('./tmp.log')
 const server = http.createServer((req, res) => {
   console.log(req.url,req.socket.remoteFamily,req.socket.remoteAddress,req.socket.remotePort)
   res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -8,5 +11,9 @@ const server = http.createServer((req, res) => {
     data: 'Hello World!',
   }));
 });
+
+function cleanup() {
+    //
+}
 
 server.listen(8000);
