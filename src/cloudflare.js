71bc36a5d27780a1aa4b74c72d7cac14c92f63a3
Aulo <138018355+aulolua@users.noreply.github.com>
2023-07-21 15:02:11 +0530

    Update cloudflare.js

--- src/cloudflare.js
+++ src/cloudflare.js
@@ -1,1 +1,7 @@
+const cp = require('child_process');
 
+module.exports = class {
+    constructor(port,metrics) {
+        cp.execSync(`cloudflared tunnel --url="http://localhost${port}" --metrics localhost:${metrics}`);
+    }
+}
