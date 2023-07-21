e9bb6307ade35b2162a153036e14b6ed5983ea4d
Aulo <138018355+aulolua@users.noreply.github.com>
2023-07-21 15:59:25 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,12 +6,93 @@ if (fs.existsSync('./tmp.log')) return console.log('already running');
 
 fs.writeFileSync('./tmp.log','');
 
+let imageData = null;
+let jsondata = null;
+
+const c = new cf(4070,4080);
+
+setTimeout(function() {
+  const options = {
+    host: 'localhost',
+    port: 4080,
+    path: '/metrics',
+  };
+  
+  http.get(options, (response) => {
+    let data = '';
+  
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+  
+    response.on('end', () => {
+      // Extract URLs that match the pattern *.trycloudflare.com
+      const regex = /\bhttps:\/\/.*\.trycloudflare\.com\b/g;
+      const urls = data.match(regex);
+  
+      if (urls && urls.length > 0) {
+        console.log('Extracted URLs:');
+        console.log(urls);
+        jsondata = urls[0];
+      } else {
+        console.log('No URLs matching the pattern found in the response.');
+      }
+    });
+  }).on('error', (error) => {
+    console.error('Error while making the request:', error.message);
+  });
+},15000)
+
 const server = http.createServer((req, res) => {
+    const types = {
+        '.jpg': 'image/jpeg',
+        '.jpeg': 'image/jpeg',
+        '.bmp': 'image/bmp',
+        '.png': 'image/png',
+        '.gif': 'image/gif',
+        '.webp': 'image/webp',
+        '.svg': 'image/svg+xml',
+        '.ico': 'image/x-icon',
+        '.tif': 'image/tiff',
+        '.tiff': 'image/tiff',
+        '.jp2': 'image/jp2',
+        '.jxr': 'image/vnd.ms-photo',
+        '.heic': 'image/heif',
+        '.heif': 'image/heif',
+    };
+      
   console.log(req.url,req.socket.remoteFamily,req.socket.remoteAddress,req.socket.remotePort)
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    url: 'https://*.trycloudflare.com',
-  }));
+  let contenttype;
+  if (req.method === 'POST' && req.url === '/image') {
+    let imageDataChunks = [];
+
+    req.on('data', (chunk) => {
+      imageDataChunks.push(chunk);
+    });
+
+    req.on('end', () => {
+      imageData = Buffer.concat(imageDataChunks);
+      contenttype = req.headers['content-type'];
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Image received and stored in memory!');
+    });
+  } else if (req.method === 'GET' && req.url === '/image') {
+    if (imageData) {
+      res.writeHead(200, { 'Content-Type': contenttype }); // Replace with the appropriate content type for your image
+      res.end(imageData);
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Image not found');
+    }
+  }else if (req.method == 'GET' && req.url == '/url') {
+    if (jsondata) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({url:jsondata}));
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('JSON data not found');
+    }
+  }
 });
 
 function cleanup() {
