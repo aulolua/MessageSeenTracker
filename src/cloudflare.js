6b2ae803d16c3265b4f07b9321800036a74bedab
Aulo <138018355+aulolua@users.noreply.github.com>
2023-08-06 14:16:45 +0530

    Delete cloudflare.js

--- src/cloudflare.js
+++
@@ -1,7 +0,0 @@
-const cp = require('child_process');
-
-module.exports = class {
-    constructor(port,metrics) {
-        cp.execSync(`cloudflared tunnel --url="http://localhost${port}" --metrics localhost:${metrics}`);
-    }
-}
